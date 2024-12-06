b290064dc9093e9d21af150c30b1711caf9f99a5
Pedr00ga <plgonzales4760@gmail.com>
2024-12-06 20:57:36 -0300

    Enviando primeiro código feito em Java para testes IDE IntelliJ

---
+++ HelloWorld/src/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int numero = 4;
+
+        if (numero >=5){
+            System.out.println("Parabéns você foi aprovado");
+        }
+        else{
+            System.out.println("Infelizmente você foi reprovado");
+        }
+    }
+}
